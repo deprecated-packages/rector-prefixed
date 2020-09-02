c5363d223895abbbf5e17a1fee21f43305a126c7
TomasVotruba <tomas.vot@gmail.com>
2020-09-02 14:26:25 +0200

    [ci] add include/exclude test

---
+++ fixture-dead-code/Excluded/ExcludeThisClass.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorPrefixeyd\FixtureDeadCode\Excluded;
+
+final class ExcludeThisClass
+{
+    public function unusedPrivateMethod()
+    {
+    }
+}
---
+++ fixture-dead-code/Included/IncludeThisClass.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorPrefixeyd\FixtureDeadCode\Included;
+
+final class IncludeThisClass
+{
+    public function unusedPrivateMethod()
+    {
+    }
+}
