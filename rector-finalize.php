8918775951f645ce688a70f9d96f6236eef27187
Tomas Votruba <tomas.vot@gmail.com>
2020-11-17 12:25:46 +0000

    finalize

--- rector-finalize.php
+++ rector-finalize.php
@@ -7,8 +7,6 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Rector\SOLID\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/create-rector.php', null, 'not_found');
-
     $parameters = $containerConfigurator->parameters();
 
     $parameters->set(Option::AUTOLOAD_PATHS, [
